f5ee5a068cf0aade0d0377c81aafdf08fdc9ac5a
Timur @shako Kashapov <timshako@gmail.com>
2017-09-03 00:51:59 +0300

    upd

---
+++ src/com/shako/hunter_a_mouse/GameApp.java
@@ -0,0 +1,17 @@
+package com.shako.hunter_a_mouse;
+
+/**
+ *
+ * @author Timur Kashapov aka shako03-Sep-17.
+ * @since 1.0
+ */
+public class GameApp {
+
+    /** */
+    GameApp() {
+
+        // TO-DO
+
+    } // com.shako.hunter_a_mouse.GameApp()
+
+} // com.shako.hunter_a_mouse.GameApp
---
+++ src/com/shako/hunter_a_mouse/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * @author Timur Kashapov aka shako03-Sep-17.
+ * @since 1.0
+ */
+package com.shako.hunter_a_mouse;
