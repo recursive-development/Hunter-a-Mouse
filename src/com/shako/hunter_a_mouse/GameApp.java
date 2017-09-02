164cae71ce232629e429c64f0a078ff3111310d9
Timur @shako Kashapov <timshako@gmail.com>
2017-09-03 00:56:41 +0300

    upd

--- src/com/shako/hunter_a_mouse/GameApp.java
+++ src/com/shako/hunter_a_mouse/GameApp.java
@@ -1,17 +1,25 @@
 package com.shako.hunter_a_mouse;
 
+ import com.badlogic.gdx.Game;
+
 /**
  *
- * @author Timur Kashapov aka shako03-Sep-17.
+ * @author Timur Kashapov aka shako.
  * @since 1.0
+ * 03-Sep-17
  */
-public class GameApp {
+public class GameApp extends Game{
 
     /** */
     GameApp() {
 
         // TO-DO
 
-    } // com.shako.hunter_a_mouse.GameApp()
+    } // GameApp()
 
-} // com.shako.hunter_a_mouse.GameApp
+
+    @Override
+    public void create() {
+
+    }
+} // GameApp
