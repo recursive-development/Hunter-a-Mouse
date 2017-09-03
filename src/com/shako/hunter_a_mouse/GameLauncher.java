5ceedc73f49f890bd33efa57842a1b0e8811227b
Timur @shako Kashapov <timshako@gmail.com>
2017-09-03 03:38:47 +0300

    complete

--- src/com/shako/hunter_a_mouse/GameLauncher.java
+++ src/com/shako/hunter_a_mouse/GameLauncher.java
@@ -1,10 +1,22 @@
 package com.shako.hunter_a_mouse;
 
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+
 /**
+ * Класс запуска игрового приложения.
+ *
  * @author Timur Kashapov aka shako.
  * @since 1.0
  * 03-Sep-17
  */
 public class GameLauncher {
-    
+
+    /** */
+    public static void main(String[] args) {
+
+        GameApp game =  new GameApp();
+        LwjglApplication launcher = new LwjglApplication(game);
+
+    } // main()
+
 } // GameLauncher
