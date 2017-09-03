14c859327006a8e7fd813772d6a69766982fbf1f
Timur @shako Kashapov <timshako@gmail.com>
2017-09-03 03:38:36 +0300

    complete

--- src/com/shako/hunter_a_mouse/GameApp.java
+++ src/com/shako/hunter_a_mouse/GameApp.java
@@ -42,24 +42,31 @@ public class GameApp extends Game{
 
     } // GameApp()
 
-
+    /** */
     @Override
     public void create() {
 
         //
         //
         batch = new SpriteBatch();
-        win = false;
 
-        mouseTexture = new Texture(Gdx.files.internal("assets/mouse.png"));
+        //
+        //
+        mouseTexture  = new Texture(Gdx.files.internal("assets/mouse.png"));
         cheeseTexture = new Texture(Gdx.files.internal("assets/cheese01.png"));
-        floorTexture = new Texture(Gdx.files.internal("assets/floor.png"));
-        winMessage   = new Texture(Gdx.files.internal("assets/winner.png"));
+        floorTexture  = new Texture(Gdx.files.internal("assets/floor.png"));
+        winMessage    = new Texture(Gdx.files.internal("assets/winner.png"));
 
-        mouseX       = 20;
-        mouseY       = 20;
-        cheeseX      = 400;
-        cheeseY      = 300;
+        //
+        //
+        mouseX        = 20;
+        mouseY        = 20;
+        cheeseX       = 400;
+        cheeseY       = 300;
+
+        //
+        //
+        win           = false;
 
 
     } // create()
@@ -70,19 +77,23 @@ public class GameApp extends Game{
 
         // Перемещение героя игры.
         //
-        if (Gdx.input.isKeyPressed(Input.Keys.UP))    mouseY--;
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN))  mouseY++;
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT))  mouseX--;
+        if (Gdx.input.isKeyPressed(Input.Keys.UP))    mouseY++;
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN))  mouseY--;
         if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) mouseX++;
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT))  mouseX--;
 
         // Непонятное условие ...
         //
         if (mouseX > cheeseX && ( mouseY > cheeseY )
                 && ( mouseX + mouseTexture.getWidth()  < cheeseX + cheeseTexture.getWidth() )
                 && ( mouseY + mouseTexture.getHeight() < cheeseY + cheeseTexture.getHeight() ))
+        {
             win = true;
+            System.out.println("Win");
+        }
 
-        // Очиста экрана и рисование графики.
+
+        // Очиста экрана сплошным цветом и рисование графики при каждой отрисовке.
         //
         Gdx.gl.glClearColor(0.8f, 0.8f, 1.0f, 1.0f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
@@ -92,8 +103,8 @@ public class GameApp extends Game{
         //-------------------------------------------
         batch.begin();
         batch.draw(floorTexture,0, 0);
-        batch.draw(cheeseTexture, cheeseX, cheeseY);
-        batch.draw(mouseTexture,   mouseX,  mouseY);
+        batch.draw(cheeseTexture, cheeseX, cheeseY, 100, 100);
+        batch.draw(mouseTexture,   mouseX,  mouseY, 100, 100);
 
         if (win) batch.draw(winMessage, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
