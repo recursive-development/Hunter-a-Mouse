08e36eea54eb2a654096b94d9e24c74cb3896845
Timur @shako Kashapov <timshako@gmail.com>
2017-09-03 19:03:21 +0300

    Добавил тестовую текстуру

--- src/com/shako/hunter_a_mouse/GameApp.java
+++ src/com/shako/hunter_a_mouse/GameApp.java
@@ -20,6 +20,9 @@ public class GameApp extends Game{
 
     /** */
     private Texture
+
+            testTexture,
+
             mouseTexture,
             floorTexture,
             cheeseTexture,
@@ -27,10 +30,18 @@ public class GameApp extends Game{
 
     /** */
     private float
+
+            testX,
+            testY,
+            testVelosity,
+
             mouseX,
             mouseY,
             cheeseX,
-            cheeseY;
+            cheeseY,
+
+            mouseVelosity;
+
 
     /** */
     private boolean win;
@@ -48,7 +59,18 @@ public class GameApp extends Game{
 
         //
         //
+        Gdx.graphics.setResizable(false);
+
+        //
+        //
         batch = new SpriteBatch();
+
+        //
+        //
+        testTexture = new Texture(Gdx.files.internal("assets/testing/test-square.png"));
+        testX         = 0.0f;
+        testY         = 0.0f;
+        testVelosity  = 5.0f;
 
         //
         //
@@ -59,10 +81,12 @@ public class GameApp extends Game{
 
         //
         //
-        mouseX        = 20;
-        mouseY        = 20;
-        cheeseX       = 400;
-        cheeseY       = 300;
+        mouseX        = 20.0f;
+        mouseY        = 20.0f;
+        cheeseX       = 400.0f;
+        cheeseY       = 300.0f;
+
+        mouseVelosity = 5.0f;
 
         //
         //
@@ -77,20 +101,16 @@ public class GameApp extends Game{
 
         // Перемещение героя игры.
         //
-        if (Gdx.input.isKeyPressed(Input.Keys.UP))    mouseY++;
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN))  mouseY--;
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) mouseX++;
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT))  mouseX--;
+        //moveMouse();
 
-        // Непонятное условие ...
+        // Тестирование перемещения посредством тестовой текстуры.
         //
-        if (mouseX > cheeseX && ( mouseY > cheeseY )
-                && ( mouseX + mouseTexture.getWidth()  < cheeseX + cheeseTexture.getWidth() )
-                && ( mouseY + mouseTexture.getHeight() < cheeseY + cheeseTexture.getHeight() ))
-        {
-            win = true;
-            System.out.println("Win");
-        }
+        moveTestTexture();
+
+        // Вывод статистики в консоль.
+        //
+        showStatistics();
+
 
 
         // Очиста экрана сплошным цветом и рисование графики при каждой отрисовке.
@@ -105,6 +125,7 @@ public class GameApp extends Game{
         batch.draw(floorTexture,0, 0);
         batch.draw(cheeseTexture, cheeseX, cheeseY, 100, 100);
         batch.draw(mouseTexture,   mouseX,  mouseY, 100, 100);
+        batch.draw(testTexture, testX, testY);
 
         if (win) batch.draw(winMessage, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
@@ -114,6 +135,73 @@ public class GameApp extends Game{
 
 
     } // render()
+
+    /** */
+    private void moveMouse() {
+
+        //
+        //
+        if (Gdx.input.isKeyPressed(Input.Keys.UP))    mouseY += 5.0f;
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN))  mouseY -= 5.0f;
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) mouseX += 5.0f;
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT))  mouseX -= 5.0f;
+
+        if( mouseX <= 0 ) mouseX = 0;
+        //if( mouseX >= ...  - mouseTexture.getWidth() ) mouseX = ... - mouseTexture.getWidth();
+
+        if ( mouseY  <= 0 ) mouseY = 0; // mouseTexture.getHeight()
+        //if ( mouseY >= ... - mouseTexture.getHeight() ) mouseY = mouseTexture.getHeight();
+
+        // Столкновение с текстурой сыра
+        //
+        if (mouseX >= cheeseX && ( mouseX + mouseTexture.getWidth()  < cheeseX + cheeseTexture.getWidth() )
+                && ( mouseY >= cheeseY ) && ( mouseY + mouseTexture.getHeight() < cheeseY + cheeseTexture.getHeight() ))
+        {
+            win = true;
+            System.out.println("Win");
+        }
+    } // moveMouse()
+
+    /** */
+    private void moveTestTexture() {
+
+        //
+        //
+        if ( Gdx.input.isKeyPressed( Input.Keys.MINUS ) ) testVelosity--;
+        if (  testVelosity <= 0 ) testVelosity = 0.5f;
+
+        if ( Gdx.input.isKeyPressed( Input.Keys.PLUS  ) ) testVelosity++;
+        if ( testVelosity >= 100.0f ) testVelosity = 100.0f;
+
+        //
+        //
+        if (Gdx.input.isKeyPressed(Input.Keys.UP))    testY += testVelosity;
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN))  testY -= testVelosity;
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) testX += testVelosity;
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT))  testX -= testVelosity;
+
+        if( testX <= 0 ) testX = 0;
+        if( testX >= 640 - testTexture.getWidth() ) testX = 640 - testTexture.getWidth();
+
+        if ( testY  <= 0 ) testY = 0; // mouseTexture.getHeight()
+        if ( testY >= 480 - testTexture.getHeight() ) testY = 480 - testTexture.getHeight();
+
+        // Столкновение с текстурой сыра
+        //
+        // if (mouseX >= cheeseX && ( mouseX + mouseTexture.getWidth()  < cheeseX + cheeseTexture.getWidth() )
+        //         && ( mouseY >= cheeseY ) && ( mouseY + mouseTexture.getHeight() < cheeseY + cheeseTexture.getHeight() ))
+        // {
+        //     win = true;
+        //     System.out.println("Win");
+        // }
+    }
+
+    /** */
+    private void showStatistics() {
+
+        System.out.printf("test [%.1f][%.1f]:[%.1jrf]\n", testX, testY, testVelosity);
+
+    } // showStatistics()
 } // GameApp
 
 
